c83771fab3a10b4699139b85eb80bd693917ebab
Jhmeel <muhammadjhmeel01@gmail.com>
2024-06-27 03:16:48 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from 'tailwindcss';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-  },
-  build: {
-    ssr: true,
-  },
-  server: {
-    middlewareMode: true,
   }
 });
