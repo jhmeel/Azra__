16ddd6acef3bdc24b52a76956d784a756892683f
Jhmeel <muhammadjhmeel01@gmail.com>
2024-06-27 06:00:40 +0100

    changes

--- src/client/src/utils/axiosInstance.ts
+++ src/client/src/utils/axiosInstance.ts
@@ -6,7 +6,7 @@ interface AxiosInstanceConfig extends AxiosRequestConfig {
   headers: Record<string, string>;
 }
 
-const baseUrl = import.meta.env.NODE_ENV=="production"?'https://distinct-reward-nosy-attraction-beta.pipeops.app/':'http://localhost:3000',
+const baseUrl = import.meta.env.NODE_ENV=="production"?'https://distinct-reward-nosy-attraction-beta.pipeops.app/':'http://localhost:3000';
 const axiosInstance = (token?: string, role?: Role): AxiosInstance => {
   const config: AxiosInstanceConfig = {
     baseURL: baseUrl,
